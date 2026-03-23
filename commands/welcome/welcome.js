e8d0a02849364a2294359dc46e32aa0f0a5b6705
ronovo <ian.kent91@gmail.com>
2026-03-23 00:18:54 +0000

    Fixing main

--- commands/welcome/welcome.js
+++ commands/welcome/welcome.js
@@ -1,14 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-//Add to On-Join
 module.exports = {
-	data: new SlashCommandBuilder().setName('Welcome').setDescription('Welcome Message').addChoices(
-        { name: 'Player', value: 'Someone looking to go on an adventure' },
-        { name: 'DM/GM', value: 'Someone looking to run an adventure' },
-    ),
+	data: new SlashCommandBuilder().setName('welcome').setDescription('welcome'),
 	async execute(interaction) {
-		await interaction.reply('Welcome to the server, (This User). I hope you are ready to game!\n'+
-                                'I am a'
-        );
+		await interaction.reply('Welcome, (This User)! Hope you are ready to game!');
 	},
 };
