7faacf9998072f0d14b7f6d09971fa396cd801d6
ronovo <ian.kent91@gmail.com>
2026-03-23 01:40:00 +0000

    fixing list

--- commands/gamesystem/list.js
+++ commands/gamesystem/list.js
@@ -1,8 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 //TODO : Add ROLES to server DM and PLAYER
-//TODO : Allow for this command to accept the parameter
-//TODO : Make a handler for each ROLE : DM and PLAYER
 module.exports = {
 	data: new SlashCommandBuilder().setName('list').setDescription('Gets list of (type)').addStringOption(option =>
 		option.setName('list_type')
